ddd91a9fbfad72457b1004c8269e66a2ba8f2131
EC2 Default User <ec2-user@ip-172-31-24-120.us-east-2.compute.internal>
2020-05-20 11:45:41 +0000

    Update UI, declare functions

--- script.js
+++ script.js
@@ -5,3 +5,35 @@
   - Create a function that accepts player and bot response and returns winner
   - Create a game function that handles results best out of 5 rounds with an end-game function and reset-game function
 */
+
+// Initialize variables
+let userWins = 0;
+let botWins = 0;
+let round = 1;
+
+// Grab document elements and call functions
+// TODO
+
+function game() {
+    // TODO
+}
+
+// Returns computer's response
+function computerPlay() {
+    let botPlay = 'ROCK';
+    const n = Math.random();
+    if (n <= 1/3) {
+        botPlay = 'PAPER';
+    } else if ((n > 1/3) && (n <= 2/3)) {
+        botPlay = 'SCISSORS';
+    }
+    return botPlay;
+}    
+
+function playRound(playerSelection, computerSelection) {
+    // TODO
+}
+
+function updateText() {
+    // TODO
+}
