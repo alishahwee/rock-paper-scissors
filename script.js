811c6b27791e9b52cb8ff1f29af003b92f7acce3
alishahwee <alisha.hwee@outlook.com>
2020-05-19 21:19:12 -0500

    Add comments

---
+++ script.js
@@ -0,0 +1,7 @@
+/* TODO:
+  - Initialize buttons to event listeners 
+  - Create a function to generate bot response
+  - Set player response variable to appropriate return value
+  - Create a function that accepts player and bot response and returns winner
+  - Create a game function that handles results best out of 5 rounds with an end-game function and reset-game function
+*/
